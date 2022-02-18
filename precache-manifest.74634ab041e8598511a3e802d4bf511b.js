0329263fc30874e376f6b156257fa64c1c24941d
codessi <johansuh@gmail.com>
2022-02-18 11:29:48 -0800

    Updates

---
+++ precache-manifest.74634ab041e8598511a3e802d4bf511b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a04d29709dded8700f988ec49fa718",
+    "url": "/grocerylist/index.html"
+  },
+  {
+    "revision": "08cee3b15771de0b4ac6",
+    "url": "/grocerylist/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/grocerylist/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocerylist/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08cee3b15771de0b4ac6",
+    "url": "/grocerylist/static/js/main.0048548d.chunk.js"
+  },
+  {
+    "revision": "d19fefb3e8b3959d6d3f",
+    "url": "/grocerylist/static/js/runtime-main.05a923ac.js"
+  }
+]);
